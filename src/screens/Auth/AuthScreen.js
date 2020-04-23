b7c4257b93090148f5d8683d1c699ec55a04e497
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2020-04-23 22:32:17 +0530

    final keyboard fix

--- src/screens/Auth/AuthScreen.js
+++ src/screens/Auth/AuthScreen.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useState } from 'react';
-import { StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Keyboard } from 'react-native';
 import FlexedContainer from '../../reusables/components/Containers/FlexedContainer';
 import { borderStyles } from '../../reusables/styles/style';
 import LoginForm from './Login/LoginForm';
@@ -9,12 +9,26 @@ import SignupForm from './Signup/SignupForm';
 const AuthScreen = (props) => {
   const [ABStyle, setABStyle] = useState(false);
 
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidHide', _keyboardDidHide);
+
+    // cleanup function
+    return () => {
+      Keyboard.removeListener('keyboardDidHide', _keyboardDidHide);
+    };
+  }, []);
+
+  const _keyboardDidHide = () => {
+    setTimeout(() => setABStyle(false), 0);
+  };
+
+
   return (
     <FlexedContainer
       contStyle={[
         borderStyles.bw_1,
         { borderColor: 'yellow' },
-        ABStyle ? { position: 'absolute', bottom: StatusBar.currentHeight } : null,
+        ABStyle ? { justifyContent: 'flex-end' } : null,
       ]}
     >
       <LoginForm setABStyle={setABStyle} />
--- src/screens/Auth/Signup/SignupForm.js
+++ src/screens/Auth/Signup/SignupForm.js
@@ -10,7 +10,6 @@ const SignupForm = (props) => (
     <Title title="Sign Up" />
     <Form
       formConfig={getFields({ onFocus: () => props.setABStyle(true) })}
-      // formConfig={getFields()}
       validateForm={validate}
     />
     <Button title="Sign Up" />
@@ -20,12 +19,6 @@ const SignupForm = (props) => (
 const styles = StyleSheet.create({
   container: {
     marginTop: 16,
-    // borderWidth: 1,
-    // flex: 1,
-    // justifyContent: 'flex-end',
-    // backgroundColor: '#fff',
-    // zIndex: 2,
-    // position: 'absolute',
   },
 });
 
